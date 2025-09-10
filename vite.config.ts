c98ea05dabd345884977661f631d3a9832a535d7
Adrian-2507 <adriang2507@gmail.com>
2025-09-10 11:12:30 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,22 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig({
+  // Set base to your repo name for GitHub Pages!
   base: "/raaganjali-fusion-site/",
   build: {
-    outDir: "docs",
+    outDir: "docs", // Output build files to docs folder for GitHub Pages
   },
-  plugins: [react(), componentTagger()],
+  plugins: [
+    react(),
+    componentTagger()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: "::",
+    port: 8080,
+  }
 });
