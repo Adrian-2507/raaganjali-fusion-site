aea8bec97ed509c32477a3234f7cae25b4826733
Adrian-2507 <adriang2507@gmail.com>
2025-09-10 11:28:55 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-export default defineConfig({
-  // Set base to your repo name for GitHub Pages!
-  base: "/raaganjali-fusion-site/",
-  build: {
-    outDir: "docs", // Output build files to docs folder for GitHub Pages
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
   },
   plugins: [
     react(),
-    componentTagger()
-  ],
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: "::",
-    port: 8080,
-  }
-});
+}));
