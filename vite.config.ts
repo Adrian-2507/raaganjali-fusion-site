2a272b6080ee35e5676ea22bdc494f65bf2deca9
Adrian-2507 <adriang2507@gmail.com>
2025-09-10 11:05:41 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig({
+  base: "/raaganjali-fusion-site/",
+  build: {
+    outDir: "docs",
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), componentTagger()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
